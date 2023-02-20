6b59c82fb1a69df42fe2067239ca195d1ebe09e0
Mehdi Aberkane <49305433+mehdiabkn@users.noreply.github.com>
2023-02-20 14:19:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ from datetime import datetime
 
 # Set up the title and date selector
 st.title("Sunburst of tatata")
-selected_date = st.date_input("Select a date", datetime.today())
+#selected_date = st.date_input("Select a date", datetime.today())
 
 # Create the sunburst chart
-fig = px.sunburst(
-    names=["tatata", "foo", "bar", "baz", "qux", "quux"],
-    parents=["", "tatata", "tatata", "foo", "foo", "bar"],
-    values=[10, 5, 3, 2, 2, 1],
-)
-st.plotly_chart(fig)
+#fig = px.sunburst(
+#    names=["tatata", "foo", "bar", "baz", "qux", "quux"],
+#    parents=["", "tatata", "tatata", "foo", "foo", "bar"],
+#    values=[10, 5, 3, 2, 2, 1],
+#)
+#st.plotly_chart(fig)
