2caf5eb43366594f4484033ecd901c676bb24f4d
Mehdi Aberkane <49305433+mehdiabkn@users.noreply.github.com>
2023-02-20 14:06:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
-st.title('Uber pickups in NYC')
+import streamlit as st
+import plotly.express as px
+from datetime import datetime
+
+# Set up the title and date selector
+st.title("Sunburst of tatata")
+selected_date = st.date_input("Select a date", datetime.today())
+
+# Create the sunburst chart
+fig = px.sunburst(
+    names=["tatata", "foo", "bar", "baz", "qux", "quux"],
+    parents=["", "tatata", "tatata", "foo", "foo", "bar"],
+    values=[10, 5, 3, 2, 2, 1],
+)
+st.plotly_chart(fig)
